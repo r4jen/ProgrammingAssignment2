c2624415b5f54b41297548e31b63bf00dd47a37d
r4jen <rajenkapadia@gmail.com>
2014-12-17 16:04:31 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+    #cache defined
+    set<- function(y){
+        x <<- y ## the matrix Y is assigned to x
+        m <<- NULL ## m is assigned as NULL in the parent environment
+    }
+      get<- function() x 
+      setinverse <- function(inverse) m <<- inverse #let m be euqal to 
+      ## the inverse of the matrix x
+      getinverse <- function() m #this returns the cached inverse of x
+      list(set=set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 
 }
 
@@ -12,4 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          m <- x$getinverse()
+          ##if there is a cached value return it
+      if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+          ##other solve the matrix and store it to the cache
+          
+      }
+      data <- x$get()
+      m<- solve(data, ...)
+      x$setinverse(m)
+      m
 }
+##Matrix has to be square for solve function to invert it other wise error will occur
